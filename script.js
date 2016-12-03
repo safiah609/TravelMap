0ab43bfdb60edbdcfa1e024807aedfa9c663b6cb
Safia Hassan <safiah_609@hotmail.com>
2016-12-02 18:44:53 -0700

    Drawing US map and displaying a point for all international airports. Connecting points between two lines--need to create a list of all destination paths from source.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var all_us_intl_airpoots;
 var lat_long = [];
 var unique_codes = [];
 var valid_codes = [];
+var SLC_average = [];
 
 function drawMap (all_us_intl_airpoots) {
     var w = 1500;
@@ -15,6 +16,22 @@ function drawMap (all_us_intl_airpoots) {
         .projection(projection);
 
 
+    var path = d3.geoPath()
+        .projection(projection);
+
+    var places = {
+        SLC: ["-111.977772", "40.788389"],
+        COL: ["-104.673178", "39.861656"]
+    };
+
+    var route = {
+        type: "LineString",
+        coordinates: [
+            places.SLC,
+            places.COL
+        ]
+    };
+
     //Define quantize scale to sort data values into buckets of color
     var color = d3.scaleQuantize()
         .range(["rgb(237,248,233)", "rgb(186,228,179)", "rgb(116,196,118)", "rgb(49,163,84)", "rgb(0,109,44)"]);
@@ -23,17 +40,15 @@ function drawMap (all_us_intl_airpoots) {
         .append("svg")
         .attr("width", w)
         .attr("height", h);
+
+    // svg.append("path")
+    //     .datum(graticule)
+    //     .attr("class", "graticule")
+    //     .attr("d", path);
+
+
     //Load in agriculture data
     d3.csv("data/us-states.json", function (data) {
-        //Set input domain for color scale
-        color.domain([
-            d3.min(data, function (d) {
-                return d.value;
-            }),
-            d3.max(data, function (d) {
-                return d.value;
-            })
-        ]);
         //Load in GeoJSON data
         d3.json("data/us-states.json", function (json) {
             //Merge the ag. data and GeoJSON
@@ -63,18 +78,24 @@ function drawMap (all_us_intl_airpoots) {
                 .attr("class", "feature")
                 .attr("d", path)
                 .style("fill", function (d) {
-                    //                        //Get data value
-                    //                        var value = d.properties.value;
-                    //
-                    //                        if (value) {
-                    //                            //If value exists…
-                    //                            return color(value);
-                    //                        } else {
-                    //                            //If value is undefined…
-                    //                            return "#ccc";
-                    //                        }
                     return "#e5e5ff"
                 });
+
+            // for (var i=0; i<2; i++){
+            //     if(i==0){
+            //         svg.append("path")
+            //             .datum({type: "LineString", coordinates: [[-111.977772, 40.788389], [-104.673178, 39.861656]]})
+            //             .attr("class", "arc")
+            //             .attr("d", path);
+            //     }
+            //     if(i==1){
+            //         svg.append("path")
+            //             .datum({type: "LineString", coordinates: [[-111.977772, 40.788389], [-115.15225, 36.080056]]})
+            //             .attr("class", "arc")
+            //             .attr("d", path);
+            //     }
+            // }
+
 
             // points
             // aa = [-111.908333, 40.762778];
@@ -83,7 +104,7 @@ function drawMap (all_us_intl_airpoots) {
             l.push(all_us_intl_airpoots[0])
             // add circles to svg
             // console.log("len of data is "+all_us_intl_airpoots.length)
-            svg.selectAll("circle")
+            var circles= svg.selectAll("circle")
                 .data(all_us_intl_airpoots).enter()
                 .append("circle")
                 .attr("cx", function (d) {
@@ -109,55 +130,97 @@ function drawMap (all_us_intl_airpoots) {
                         }
                     }
                 })
-                .attr("r", "4px")
-                .attr("fill", "red")
-                .on("click", function () {
-                    console.log("CLICKED")
-                })
-                .on("mouseover", function () {
-                    console.log("HOVERING")
-                })
+                .attr("r", "6px")
+                .attr("fill", "red");
+                // .on("click", function (d) {
+                //     console.log("CLICKED")
+                // })
+                // .on("mouseover", function (d) {
+                //     console.log(d)
+                // })
+            circles
+                .on("mouseover", function (d) {
+
+                    for (var i=0; i<2; i++){
+                        if(i==0){
+                            svg.append("path")
+                                .datum({type: "LineString", coordinates: [[-111.977772, 40.788389], [-104.673178, 39.861656]]})
+                                .attr("class", "arc")
+                                .attr("d", path);
+                        }
+                        if(i==1){
+                            svg.append("path")
+                                .datum({type: "LineString", coordinates: [[-111.977772, 40.788389], [-115.15225, 36.080056]]})
+                                .attr("class", "arc")
+                                .attr("d", path);
+                        }
+                    }
+                 })
+                .on("mouseleave", function (d) {
+                    d3.selectAll(".arc").remove();
+                });
         });
+
     });
 }
 
 
+function generateLineChart(SLC_average) {
+    console.log("inside generate lines")
+    var margin = {top:20, right:20, bottom:40, left:20},
+        width = 900 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
 
-// /**
-//  * Returns the Latitude and Longitude of an airport based on it's IATA Code.
-//  *
-//  * @param IATA_Code
-//  * @returns {[*,*]}
-//  */
-// var getCoordinates = function getCoordinates(IATA_Code) {
-//     var airport = csv_airports.filter(function (d) {
-//         return d.IATA_Code === IATA_Code;
-//     });
-//
-//     if (airport.length === 0) {
-//         throw "Unknown IATA Code: " + IATA_Code;
-//     } else if (airport.length > 1) {
-//         throw "Duplicate IATA Codes Found: " + IATA_Code;
-//     }
-//
-//     return [airport[0].Lat, airport[0].Long];
-// };
-//
-// var airportStatsChart = new AirportStatsChart();
-//
-// var brushChart = new BrushChart(on_time_performance);
-//
-//
-// // Convert Object Array into an easily searchable string array:
-// var us_intl_airport_CODES = us_international_airports.map(function (d) {
-//     return d.code;
-// });
-// // Filter to just United States and only International Airports
-// // NOTE: The "United States" check is most likely redundant because the csv file with
-// // the list of international airports technically only has US airports.
-// var US_intl_airports = csv_airports.filter(function (d) {
-//     return (us_intl_airport_CODES.indexOf(d.IATA_Code) >= 0 && d.Country === "United States" );
-// });
+    var svg = d3.select("#l1")
+        .append("svg")
+        .attr("width",width + margin.left + margin.right)
+        .attr("height",height + margin.top + margin.bottom)
+        g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+    var x = d3.scaleTime()
+        .rangeRound([0, width]);
+
+    var y = d3.scaleLinear()
+        .rangeRound([height, 0]);
+
+    var line = d3.line()
+        .x(function(d){
+            // console.log(d.FlightDate)
+            return x(d.FlightDate);
+        })
+        .y(function(d){
+            console.log(d.DepDelay)
+            return y(d.DepDelay);
+        });
+
+    x.domain(d3.extent(SLC_average, function(d) { return d.FlightDate; }));
+    y.domain(d3.extent(SLC_average, function(d) { return d.DepDelay; }));
+
+    g.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+    g.append("g")
+        .attr("class", "axis axis--y")
+        .call(d3.axisLeft(y))
+        .append("text")
+        .attr("fill", "#000")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", "0.71em")
+        .style("text-anchor", "end")
+        .text("Delay (minutes)");
+
+    g.append("path")
+        .datum(SLC_average)
+        .attr("class", "line")
+        .attr("d", line);
+
+}
+
+
 
 // Load CSV files
 d3.csv("data/us_international_airports.csv", function (error, csv) {
@@ -206,3 +269,25 @@ d3.csv("data/airports.csv", function (error, csv) {
     // console.log("size of intl. airports is "+all_us_intl_airpoots.length)
     drawMap(all_us_intl_airpoots)
 });
+
+
+d3.csv("data/SLC_average.csv", function (error, csv) {
+
+    var format_time = d3.timeParse("%Y-%m-%d");
+
+    csv.forEach(function (d) {
+        d.FlightDate = format_time(d.FlightDate);
+        // d.Origin = d.Origin;
+        // d.DepDelay = d.DepDelay;
+        // d.ArrDelay = d.ArrDelay;
+        // d.CRSElapsedTime = d. CRSElapsedTime;
+        // d.ActualElapsedTime = d.ActualElapsedTime;
+    });
+
+    SLC_average = csv.filter(function (d) {
+        return new Date(d.FlightDate).getFullYear() === 1993;
+    });
+
+    console.log("length of slc_avg is "+SLC_average.length)
+    // generateLineChart(SLC_average)
+})
